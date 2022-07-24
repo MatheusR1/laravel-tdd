3ea92019b4ea3b228d67d5cb7a991be97ab4ec28
MatheusR1 <matheusrocha_456@hotmail.com>
2022-07-24 20:36:59 -0300

    first commit

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCityRequest;
+use App\Http\Requests\UpdateCityRequest;
+use App\Models\City;
+use Illuminate\Http\Response;
+
+class CityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cities = City::orderBy('id', 'desc')->paginate();
+
+        return response()->json(compact('cities'), Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCityRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCityRequest $request)
+    {
+        $city = new City($request->all());
+
+        return  response()->json(compact('city'), Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function show(City $city)
+    {
+        return response()->json(compact('city'), Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCityRequest  $request
+     * @param  \App\Models\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCityRequest $request, City $city)
+    {
+        $city = $city->update($request->all());
+
+        return response()->json(compact('city'), Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(City $city)
+    {
+        $deleted = $city->delete();
+
+        return response()->json(compact('deleted'), Response::HTTP_OK);
+    }
+}
